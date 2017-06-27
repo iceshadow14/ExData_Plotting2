44f1dd069d675b9d9c9a540a992e7301c23b5039
Jia-jie Hung <iceshadow14@gmail.com>
2017-06-27 13:47:38 +0800

    Update plot1.R
    add title

--- plot1.R
+++ plot1.R
@@ -14,7 +14,7 @@ SCC <- readRDS(file = "Source_Classification_Code.rds")
 # plot(summarySCC_PM25$Emissions, summarySCC_PM25$year)
 aggEMI <- aggregate(Emissions ~ year, EMI, sum)
 
-barplot(aggEMI$Emissions, names.arg = aggEMI$year, xlab = "Year", ylab = "Emissions")
+barplot(aggEMI$Emissions, names.arg = aggEMI$year, xlab = "Year", ylab = "Emissions", main = "Total emissions per year")
 
 ### The answer is YES
 dev.copy(png, filename="plot1.png", width=480, height=480)
